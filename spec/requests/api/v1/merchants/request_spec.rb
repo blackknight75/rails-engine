191525c71f7f8eff247c8a591aaf5918d6b4d471
blackknight75 <devbox4714@gmail.com>
2017-03-13 14:54:49 -0600

    add test for single merchant name. add test for single merchant attributes count

--- spec/requests/api/v1/merchants/request_spec.rb
+++ spec/requests/api/v1/merchants/request_spec.rb
@@ -20,5 +20,9 @@ require 'rails_helper'
       get "/api/v1/merchants/#{merchant.id}"
 
       expect(response).to be_success
+
+      merchant_attrs = JSON.parse(response.body, symbolize_names: true)
+      expect(Merchant.last.name).to eq(merchant_attrs[:name])
+      expect(merchant_attrs.count).to eq(4)
     end
   end
