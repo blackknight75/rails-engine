f482411f28bf8e7527ce6322988b15eab68a5419
blackknight75 <devbox4714@gmail.com>
2017-03-13 17:21:03 -0600

    add tests for finding merchants by attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
+      resources :merchants, only: [:index, :show] do
+      end
     end
   end
 end
