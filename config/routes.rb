eef1216bd14d25e3b0b275174b857e9c17a5ad25
blackknight75 <devbox4714@gmail.com>
2017-03-14 11:11:10 -0600

    add customer find, find_all, random.

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,13 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
       end
       resources :transactions, only: [:index, :show]
+
+      namespace :customers do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+        get '/random', to: 'random#show'
+      end
+      resources :customers, only: [:index, :show]
     end
   end
 end
