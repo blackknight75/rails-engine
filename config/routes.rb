de9628838f2724ada70b51b5f198b8c7760b3f00
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-03-13 18:55:38 -0600

    misc

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
         get 'find',      to: 'search#show'
         get 'random',    to: 'random#show'
       end
-      resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]
+      
+      resources :merchants, only: [:index, :show]
     end
   end
 end
