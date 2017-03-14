ce2f6302c4e8b64dc427c36f978468b3e644b31b
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-03-14 14:10:52 -0600

    add conditional for invoice_item_params

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::InvoiceItems::SearchController < ApplicationController
   private
 
   def invoice_item_params
+    params[:unit_price] = ((params[:unit_price].to_f)*100).round if params[:unit_price] && params[:unit_price].include?('.')
     params.permit(:id, :item_id, :invoice_id, :quantity, :created_at, :updated_at, :unit_price)
   end
 end
