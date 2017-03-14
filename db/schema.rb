92208e59342b704a484bd592545a435c57250276
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-03-13 20:46:52 -0600

    write passing model test for invoice_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314010222) do
+ActiveRecord::Schema.define(version: 20170314024337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "invoice_id"
+    t.integer  "quantity"
+    t.integer  "unit_price"
+    t.datetime "created_at", precision: 0
+    t.datetime "updated_at", precision: 0
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.integer  "customer_id"
