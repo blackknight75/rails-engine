23553df6bcc618b9a86b5b02b3af58536b4eeea0
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-03-16 10:22:27 -0600

    add model tests for transaction and customer

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  it { should validate_presence_of(:result) }
+  it { should validate_presence_of(:invoice_id) }
+  it { should validate_presence_of(:credit_card_number) }
+end
